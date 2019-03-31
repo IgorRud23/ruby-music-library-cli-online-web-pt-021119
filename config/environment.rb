16b317470ce986ec68f5c02bfc53c67b287ccedb
Igor Rud <rud.igor23@gmail.com>
2019-03-31 20:18:38 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -4,13 +4,13 @@ Bundler.require
 module Concerns
   module Findable
 
-    def self.find_by_name(name)
+    def find_by_name(name)
     self.all.find do |object|
       object.name == name
     end
     end
 
-    def self.find_or_create_by_name(name)
+    def find_or_create_by_name(name)
       self.find_by_name(name) || self.create(name)
     end
 
