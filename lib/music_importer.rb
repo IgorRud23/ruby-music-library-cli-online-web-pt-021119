1b87cb733eb63301c1afaab3d19e187960b5a679
Igor Rud <rud.igor23@gmail.com>
2019-03-31 21:30:08 +0000

    Automatically backed up by Learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -12,11 +12,9 @@ Dir.glob("#{path}/*.mp3").collect {|file| file.gsub("#{path}/", "")}
 end
 
 def import
-  new_files = self.files
-		new_files.each do |file|
+  files.each do |file|
 			new_song = Song.create_from_filename(file)
-		end 
-#Song.create_from_filename(file_name)
+		end
 end
 
 end
