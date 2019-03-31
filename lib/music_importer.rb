5090b9cbbcdd7611b317597988861e7515a3bf0f
Igor Rud <rud.igor23@gmail.com>
2019-03-31 21:27:51 +0000

    Automatically backed up by Learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -11,8 +11,8 @@ def files
 Dir.glob("#{path}/*.mp3").collect {|file| file.gsub("#{path}/", "")}
 end
 
-def import
-Song.create_from_filename(file_name) 
+def import(file_name)
+Song.create_from_filename(file_name)
 end
 
 end
